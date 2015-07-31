8624e8ac17663d3ec949a57efd4924a7a9a45cac
JeffBobbo <james.delph@hotmail.co.uk>
2015-07-31 02:03:34 +0100

    fixed quote

--- BobboBot/quote.pm
+++ BobboBot/quote.pm
@@ -5,66 +5,184 @@ package BobboBot::quote;
 use warnings;
 use strict;
 
-use BobboBot::users;
-
 use POSIX;
+use JSON qw(decode_json encode_json);
+use Switch;
 
-my $source = 'quote.list';
+use BobboBot::users;
+use BobboBot::access;
+
+
+my $file = 'quotes.json';
+my $json;
+
+
+my $styles = {
+  NORMAL => 0, # will appear IRC style: <nick> quote
+  ACTION => 1, # like an IRC action: * <nick> actions
+  ELEGANT => 2 # author is prepended using an em-dash
+};
+
+sub load
+{
+  open(my $fh, '<', $file) or return undef;
+  my @lines = <$fh>;
+  close($fh);
+
+  my $src = join('', @lines);
+  if (length($src))
+  {
+    return decode_json($src);
+  }
+  return {};
+}
+
+
+sub save
+{
+  return 0 if (!defined $json);
+
+  my $text = encode_json($json);
+  open(my $fh, '>', $file) or return 0;
+  print $fh $text;
+  close($fh);
+  return 1;
+}
 
 sub add
 {
-  my $who = shift();
-  my $where = shift();
-  my $author = shift();
-  my $quote = shift();
+  my @arg = @{$_[0]->{arg}};
+  my $who = $_[0]->{who};
 
-  if (checkAccess($who, $where) < accessLevel('op'))
+  if (checkAccess($who, $_[0]->{where}) < accessLevel('op'))
   {
     return 'Permission denied.';
   }
 
-  my $entry = "";
-  if ($author ne 'NO_AUTH')
+  my $string = join(' ', splice(@arg, 1));
+
+  my $start;
+  my $end;
+
+  my $style;
+  if (($start = index($string, '-s ')) >= 0)
   {
-    $entry .= '<' . $author . '> '
+    $end = index($string, ' -', $start + 3);
+    $style  = substr($string, $start + 3, ($end == -1 ? length($string) : $end) - ($start + 3));
   }
-  $entry .= $quote;
+  my $author;
+  if (($start = index($string, '-a')) >= 0)
+  {
+    $end = index($string, ' -', $start + 3);
+    $author = substr($string, $start + 3, ($end == -1 ? length($string) : $end) - ($start + 3));
+  }
+  my $quote;
+  if (($start = index($string, '-q')) >= 0)
+  {
+    $end = index($string, ' -', $start + 3);
+    $quote  = substr($string, $start + 3, ($end == -1 ? length($string) : $end) - ($start + 3));
+  }
 
-  open(my $fh, '>>', $source) or return 'Failed to open quote list: ' . $!;
-  print $fh $entry . "\n";
-  close($fh);
+  return "No quote given to add" if (length($quote) == 0);
 
-  return 'Added "<' . $author . '> ' . $quote . '" to the list.';
+  $style = $style ? $styles->{uc($style)} : 0;
+
+  my ($nick)  = split('!', $who);
+  my $h = {
+    style => $style,
+    author => $author,
+    quote => $quote,
+    who => $nick,
+  };
+
+  push(@{$json}, $h);
+  save();
+  return "Added '" . $quote . "' to the list and saved";
 }
 
+use Data::Dumper;
 sub run
 {
-  my @arg = @{$_[0]->{arg}};
+  $json = load() if (!defined $json);
+  return 'No quotes!' if (!defined $json);
 
+
+  my @arg = @{$_[0]->{arg}};
   my $action = shift(@arg);
-  if (defined $action && $action eq 'add')
+
+  if (defined $action && $action eq '-add')
   {
-    my $author = shift(@arg);
-    my $quote = join(' ', @arg);
-    return add($_[0]->{who}, $_[0]->{where}, $author, $quote);
+    return add($_[0]);
+  }
+  elsif (defined $action && $action eq '-styles')
+  {
+    my @a = sort(keys %{$styles});
+    my $ret = lc($a[0]);
+    for (my $i = 1; $i < @a; $i++)
+    {
+      $ret .= ', ' . lc($a[$i]);
+    }
+    return $ret;
   }
   else
   {
-    open(my $fh, '<', $source) or return 'Failed to open quote list: ' . $!;
-    my @lines = <$fh>;
-    close($fh);
+    my $a = $action;
+    my $q = shift(@arg);
 
-    if (@lines == 0)
+    my @list = @{$json};
+    if ($a && length($a))
     {
-      return 'No quotes!';
+      $a =~ s/\*/.*/g;
+      for (my $i = @list - 1; $i >= 0; $i--)
+      {
+        if ($list[$i]->{author} !~ /^$a$/)
+        {
+          splice(@list, $i, 1);
+        }
+      }
     }
-    return $lines[floor(rand(@lines))];
+    if ($q && length($q))
+    {
+      for (my $i = @list; $i >= 0; $i--)
+      {
+        if (index($list[$i]->{quote}, $q) == -1)
+        {
+          splice(@list, $i, 1);
+        }
+      }
+    }
+
+    return 'No quotes found' if (@list == 0);
+    my $i = floor(rand(@list));
+    my $quote = $list[$i];
+
+    my $resp = "";
+    switch ($quote->{style})
+    {
+      case ($styles->{NORMAL})
+      {
+        $resp = '<' . $quote->{author} . '> ' . $quote->{quote};
+      }
+      case ($styles->{ACTION})
+      {
+        $resp = '* ' . $quote->{author} . ' ' . $quote->{quote};
+      }
+      case ($styles->{ELEGANT})
+      {
+        $resp = '"' . $quote->{quote} . '" — ' . $quote->{author};
+      }
+    }
+    return $resp;
   }
 }
 
 sub help
 {
-  return '!quote [add nick quote] - Retrieve a quote from the list, or add one (requires auth)';
+  return [
+    '!quote - Retrieve a quote from the list',
+    '!quote -add -a author -q quote -s style - Add a quote to the list',
+    '!quote -styles - Retrieve the possible styles'
+  ];
 }
 
 sub auth
